2c045de297461e89aebd95c46865d60d5035e0f4
elimut <121231529+elimut@users.noreply.github.com>
2023-04-18 16:54:57 +0200

    Ajout
    code sofian

--- route/abonnements.js
+++ route/abonnements.js
@@ -40,8 +40,8 @@ const path = (app)=>{
     // endpoint création nouvel abonnement
     app.post('/abonnement', (req, res) =>{
         const {titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif} = req.body;
-        connection.query('INSERT INTO abonnement(titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif) VALUES (?,?,?,?,?,?,?,?);',[titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif], (err, results)=>{
-            if (err) throw err;
+         connection.query('INSERT INTO abonnement(titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif) VALUES (?,?,?,?,?,?,?,?);',[titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif], (err, results)=>{
+            if (err)   throw err;
             res.json(results);
         })
     })
@@ -80,3 +80,62 @@ module.exports = path;
 
 
 
+// Code sofian:
+// const {connection} = require('../server');
+
+// const path = (app)=>{
+//   app.get('/abonnement', (req, res)=>{
+//     connection.query('SELECT * FROM abonnement;', 
+//     [],
+//     (err, results)=>{
+//       if (err) throw err;
+//       res.json(results)
+//     })
+//   })
+
+//   app.get('/abonnement/:id', (req, res)=>{
+//     const id_abonnement = req.params.id;
+//     connection.query('SELECT * FROM abonnement WHERE id_abonnement = ?;', 
+//     [id_abonnement],
+//     (err, results)=>{
+//       if (err) throw err;
+//       res.json(results)
+//     })
+//   })
+
+//   app.post('/abonnement', (req, res) => {
+//     const { titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif } = req.body;
+    
+//     if (!titre) {
+//       res.status(400).json({ error: 'Le titre est obligatoire' });
+//       return;
+//     }
+  
+//     connection.query(
+//       'INSERT INTO abonnement(titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+//       [titre, tout_club, toutes_salles, tout_pays, reduc_complement, reduc_medecine, reduc_coach, tarif],
+//       (error, data) => {
+//         if (error) {
+//           console.error(error);
+//           res.status(500).send('Erreur du serveur');
+//         } else {
+//           res.status(201).json({ message: 'Abonnement créé avec succès' });
+//         }
+//       }
+//     )
+//   });
+  
+//   app.delete('/abonnement/:id', (req, res) => {
+//     const id_abonnement = req.params.id;
+//     connection.query('DELETE FROM abonnement WHERE id_abonnement = ?', [id_abonnement], (err, results) => {
+//       if (err) throw err;
+//       if(results.affectedRows === 0) {
+//         res.status(404).send('Abonnement non trouvé');
+//       } else {
+//         res.status(200).json({ message: 'Abonnement supprimé avec succès'});
+//       }
+//     });
+//   });
+// }
+
+// module.exports = path;
