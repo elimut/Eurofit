32bb4c5c8bd1f2252c510f8cee9ef5b3457bf0a8
elimut <121231529+elimut@users.noreply.github.com>
2023-04-19 15:13:30 +0200

    automatisation

--- route/abonnements.js
+++ route/abonnements.js
@@ -2,6 +2,8 @@
 const {connection} = require('../serveur');
 // récupération module connection du fichier serveur
 
+
+/*Essai de get */
 // app.get('/abonnement', (req, res)=>{
 //     connection.query('SELECT * FROM abonnement;',(err, results)=>{
 //         if (err) throw err;
@@ -10,9 +12,9 @@ const {connection} = require('../serveur');
     // requete vers table abonnement
     // query: requête; , callback
 // })
-// ou
+/*ou*/
 // app.get('/abonnement:id', (req, res)=>{
-    // binded id?
+    // binded id? rendre dynamique ?
     // connection.query('SELECT * FROM abonnement WHERE id = ?;', [id], (err, results)=>{
         // requête préparée []
         // if (err) throw err;
@@ -21,23 +23,86 @@ const {connection} = require('../serveur');
     // requete vers table abonnement
     // query: requête; , callback
 // })
-// faire un seul composant de ces deux parties -> dans fonction fléchée
+/*faire un seul composant de ces deux parties -> dans fonction fléchée*/
+
 const path = (app)=>{
-    app.get('/abonnement', (req, res)=>{
-        connection.query('SELECT * FROM abonnement;',[], (err, results)=>{
-            if (err) throw err;
-            res.json(results);
-        }); 
-    })
-    // path fonction 
+    /* path fonction, revoir*/
+/*Get pour la table abonnement*/
+    // app.get('/abonnement', (req, res)=>{
+    //     connection.query('SELECT * FROM abonnement;',[], (err, results)=>{
+    //         if (err) throw err;
+    //         res.json(results);
+    //     }); 
+    // })
+
+/*Get pour toutes tables définies dans le tableau tables, modifiable*/
+    const tables = [
+        { name: 'abonnement', query: 'SELECT * FROM abonnement;' },
+        { name: 'club', query: 'SELECT * FROM club;' },
+        { name: 'membre', query: 'SELECT * FROM membre;' }
+      ];
+
+      app.get('/:table', (req, res) => {
+        const tableName = req.params.table;
+        const table = tables.find(t => t.name === tableName);
+        /*array.find() est utilisée pour trouver un objet dans un tableau array qui a une propriété name égale à la valeur de la variable arrayName.
+        la fonction de rappel utilisée est une fonction fléchée qui prend un argument t (un élément du tableau tables) et qui retourne true si la propriété name de t est égale à la valeur de la variable tableName. La méthode array.find() retourne le premier élément dans le tableau tables qui satisfait cette condition ou undefined si aucun élément ne satisfait la condition.*/
+        if (!table) {
+          throw new Error(`La table ${tableName} n'existe pas`);
+        }
+        connection.query(table.query, [], (err, results) => {
+          if (err) {
+            throw err;
+          }
+          res.json(results);
+        });
+      }); 
+
+/*Get pour différentes tables avec if else*/
+    // app.get('/:table', (req, res)=>{
+    //     const table = req.params.table;
+    //     if (table === 'abonnement') {
+    //        requeteSqlGet = 'SELECT * FROM abonnement;' 
+    //     } else if (table === 'club') {
+    //         requeteSqlGet = 'SELECT * FROM club;' 
+    //     } else if (table === 'membre') {
+    //         requeteSqlGet = 'SELECT * FROM membre;' 
+    //     } else{
+    //         throw err;
+    //     }
+    //      connection.query(
+    //         requeteSqlGet,
+    //         [], 
+    //         (err, results)=>{
+    //         if (err)  throw err;
+    //         res.json(results);
+    //     })
+    // })
+
+/*Get pour table avec id spécifié pour afficher la ligne*/
     app.get('/abonnement/:id', (req, res)=>{
         const id_abonnement = req.params.id;
         connection.query('SELECT * FROM abonnement WHERE id_abonnement = ?;', [id_abonnement], (err, results)=>{
             if (err) throw err;
             res.json(results);
         }); 
-    })
-    // endpoint création nouvel abonnement
+    });
+    app.get('/:table/:id', (req, res) => {
+        const tableName = req.params.table;
+        const id_abonnement = req.params.id;
+        const table = tables.find(t => t.name === tableName);
+        if (!table) {
+            throw new Error(`La table ${tableName} n'existe pas`);
+          }
+          connection.query(table.query, [], (err, results) => {
+            if (err) {
+              throw err;
+            }
+            res.json(results);
+          });
+        });
+
+/* Post, endpoint création nouvel abonnement, re voir les points de terminaison*/
     app.post('/abonnement', (req, res) =>{
         const {type, prix, bilan_IMC, acces_club} = req.body;
          connection.query('INSERT INTO abonnement(type, prix, bilan_IMC, acces_club) VALUES (?,?,?,?);',[type, prix, bilan_IMC, acces_club], (err, results)=>{
@@ -55,7 +120,7 @@ const path = (app)=>{
             else{
                 res.status(200).json({ message: 'Abonnement supprimé avec succès'});
                 // res.status(200).send('Abonnement supprimé avec succès');
-                // Différence entre res.send et res.json dans Express.js: send, message sinon objet
+                // Différence entre res.send et res.json dans Express.js: send, message sur interface, notamment page HTML, plutôt destiné à l' user sinon objet
             }
         })
     })
@@ -75,6 +140,7 @@ const path = (app)=>{
         if (column === 'type') {
            requeteSql = 'UPDATE abonnement SET type = ? WHERE id_abonnement = ?;' 
            value = req.body.type
+            // récupération valeur dans body de type? à spécifier
         } else if (column === 'prix') {
             requeteSql = 'UPDATE abonnement SET prix = ? WHERE id_abonnement = ?;'
             value = req.body.prix
@@ -87,7 +153,6 @@ const path = (app)=>{
         } else{
             throw err;
         }
-        // dans body 
          connection.query(
             requeteSql,
             [value, id_abonnement], 
