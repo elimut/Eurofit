529d5ec22642887ad49add23cea052591e70201d
elimut <121231529+elimut@users.noreply.github.com>
2023-07-20 16:44:22 +0200

    maj

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ app.listen(port, ()=>{
     console.log(`Serveur is running on ${port}`);
 });
 
+// cors est un module qui facilite la gestion des requêtes Cross-Origin Resource Sharing (CORS) dans les applications web. CORS est un mécanisme de sécurité qui permet à un serveur de contrôler les requêtes provenant de domaines différents. Cela est particulièrement utile lorsque tu as une application frontend (par exemple, une application React ou Angular) qui communique avec une API backend.
+// En utilisant cors, tu peux spécifier les configurations de politique de partage des ressources (Cross-Origin Resource Sharing) et autoriser les requêtes provenant de domaines spécifiques ouvertes à ton serveur.
+// Voici comment tu peux l'utiliser dans ton code Node.js :
+// Tout d'abord, tu dois installer la bibliothèque cors en exécutant la commande npm install cors dans ton répertoire de projet.
+// Ensuite, tu dois l'importer dans ton code en utilisant require('cors').
+// Une fois que tu as importé cors, tu peux l'utiliser en tant que middleware dans Express.js. Voici comment tu peux l'utiliser :
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// n utilisant app.use(cors()), tu autorises toutes les requêtes provenant de n'importe quel domaine à accéder à tes ressources. Cela peut être pratique pendant le développement, mais tu peux également spécifier des options plus restrictives en passant un objet de configuration à cors(). Cela te permet de définir des règles spécifiques sur les origines autorisées, les méthodes HTTP permises, les en-têtes personnalisés, etc.
--- serveur.js
+++ serveur.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 3000;
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+// Cette ligne configure body-parser pour analyser les données envoyées avec les requêtes HTTP contenant des URL encodées. Cela signifie que lorsque tu envoies des données à ton serveur via une requête POST ou PUT, ces données peuvent être envoyées au format x-www-form-urlencoded, qui est un format couramment utilisé pour soumettre des formulaires HTML.
+// extended: true indique à body-parser de prendre en charge des fonctionnalités étendues du format x-www-form-urlencoded. Cela permet notamment de traiter les tableaux ou les objets complexes dans les données envoyées.
 app.use(bodyParser.json());
+// Cette ligne configure body-parser pour analyser les données envoyées avec les requêtes HTTP au format JSON. Lorsque tu envoies des données JSON avec une requête POST ou PUT, body-parser sera responsable de les analyser et de les rendre accessibles dans ton code sous forme d'objets JavaScript. En utilisant ces deux lignes de code, tu peux traiter les données envoyées dans le corps des requêtes HTTP, qu'elles soient au format x-www-form-urlencoded ou JSON. Cela te permet de récupérer facilement ces données dans ton code serveur et de les utiliser pour effectuer des opérations ou les stocker dans une base de données, par exemple.
 
 const connection = mysql.createConnection({
     host: 'localhost',
